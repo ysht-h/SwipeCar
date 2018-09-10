219f6fb6e3beac0d25a0267fd4554246ac60284e
ysht-h <>
2018-09-10 17:05:09 +0900

    first commit

---
+++ Assets/carController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carController : MonoBehaviour
+{
+
+
+    float speed = 0.0f;
+    enum SPINST
+    {
+        INIT,
+        WAIT,
+        RESULT,
+    };
+
+    SPINST spinst = SPINST.INIT;
+    Vector2 start;
+
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        switch (spinst)
+        {
+            case SPINST.INIT:
+                if (Input.GetMouseButtonDown(0))
+                {
+                    this.start = Input.mousePosition;
+                }
+                else
+                if (Input.GetMouseButtonUp(0))
+                {
+                    Vector2 end = Input.mousePosition;
+                    float swipe = end.x - start.x;
+                    this.speed = swipe / 500.0f;
+
+                    // 効果音再生する
+                    GetComponent<AudioSource>().Play();
+
+                    spinst = SPINST.WAIT;
+                }
+
+                break;
+
+            case SPINST.WAIT:
+
+                if (this.speed < 0.01f)
+                {
+                    spinst = SPINST.RESULT;
+                }
+
+                break;
+
+            case SPINST.RESULT:
+
+                if (Input.GetMouseButtonUp(0))
+                {
+                    Vector3 pos = transform.position;
+                    pos.x = -7.0f;
+                    transform.SetPositionAndRotation(pos, Quaternion.identity);
+
+                    this.speed = 0.0f;
+                    spinst = SPINST.INIT;
+                }
+
+                break;
+        }
+
+        if (spinst < SPINST.RESULT)
+        {
+            transform.Translate(this.speed, 0.0f, 0.0f);
+            this.speed *= 0.98f;
+        }
+    }
+
+}
---
+++ Assets/uiMgr.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class uiMgr : MonoBehaviour 
+{
+    GameObject car;
+    GameObject flag;
+    GameObject distance;
+
+	// Use this for initialization
+	void Start () 
+    {
+        this.car = GameObject.Find("car");
+        this.flag = GameObject.Find("flag");
+        this.distance = GameObject.Find("distance");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        float length = this.flag.transform.position.x -
+                       this.car.transform.position.x;
+
+        if (length > 0)
+        {
+            this.distance.GetComponent<Text>().text =
+                "ゴールまで" + length.ToString("F2") + "m";
+        }
+        else
+        {
+            this.distance.GetComponent<Text>().text =
+                "オーバー！　残念！！";
+        }
+	}
+}
